b8913392114ea48dadf69f98d8dfdb7dd451d30d
Pawel Sioda <pawel.sioda@gmail.com>
2024-06-16 19:50:53 +0200

    add AuthContext and AuthContext provider

---
+++ src/context/Auth/AuthContext.tsx
@@ -0,0 +1,37 @@
+import { type ReactNode, createContext, useContext, useState } from "react";
+
+type AuthContextType = {
+  isLoggedIn: boolean;
+  toggleValue: () => void;
+  logIn: () => void;
+  logOut: () => void;
+};
+
+const AuthContext = createContext<null | AuthContextType>(null);
+
+const useAuth = (): AuthContextType => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const toggleValue = () => setIsLoggedIn(!isLoggedIn);
+  const logIn = () => setIsLoggedIn(true);
+  const logOut = () => setIsLoggedIn(false);
+
+  return { isLoggedIn, toggleValue, logIn, logOut };
+};
+
+export const useAuthContext = () => {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error(
+      "useAuthContext must be used within an AuthContextProvider"
+    );
+  }
+
+  return context;
+};
+
+export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
+  return (
+    <AuthContext.Provider value={useAuth()}>{children}</AuthContext.Provider>
+  );
+};
